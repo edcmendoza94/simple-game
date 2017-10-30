00567a686ca944de91b6ff2cbfd3c66626e804e2
edcmendoza94 <edcmendoza94@gmail.com>
2017-10-30 12:03:48 -0700

    Pushing simple-game files into new repository

---
+++ src/main/java/Cell.java
@@ -0,0 +1,11 @@
+public class Cell {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+}
---
+++ src/test/java/GameboardTest.java
@@ -0,0 +1,19 @@
+/*
+ * This is a block comment
+ */
+
+import junit.framework.TestCase;
+
+public class GameboardTest extends TestCase {
+    public GameboardTest(String name) {
+        super(name);
+    }
+
+    public void testAddCell() {
+        GameBoard gameboard = new GameBoard();
+        assertEquals(0, gameboard.getCellNumber());
+        Cell cell = new Cell();
+        gameboard.addCell(cell);
+        assertEquals(1, gameboard.getCellNumber());
+    }
+}
