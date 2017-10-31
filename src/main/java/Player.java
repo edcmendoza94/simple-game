daf93061674cb04818fccab21eeff2a79c2291c6
edcmendoza94 <edcmendoza94@gmail.com>
2017-10-31 13:19:37 -0700

    Player class created

---
+++ src/main/java/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private Cell position;
+
+    public Cell getPosition() {
+        return this.position;
+    }
+
+    public void setPosition(Cell newPosition) {
+        this.position = newPosition;
+    }
+}
