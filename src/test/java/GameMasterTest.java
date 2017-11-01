8c07d107373b263f07b3e554a0cf6a86060279b4
edcmendoza94 <edcmendoza94@gmail.com>
2017-11-01 10:33:01 -0700

    Added test for verifying number of players at the start of a game

--- src/test/java/GameMasterTest.java
+++ src/test/java/GameMasterTest.java
@@ -10,4 +10,17 @@ public class GameMasterTest extends TestCase {
 
         assertSame(firstInstance, secondInstance);
     }
+
+    public void testPlayerInit() {
+        GameMaster master = GameMaster.instance();
+        master.setGameBoard(new SimpleGameBoard());
+        master.setNumberOfPlayers(6);
+        assertEquals(6, master.getNumberOfPlayers());
+        Cell go = master.getGameBoard().getCell(0);
+
+        for(int i = 0; i < 6; i++) {
+            Player player = master.getPlayer(i);
+            assertSame(go, player.getPosition());
+        }
+    }
 }
